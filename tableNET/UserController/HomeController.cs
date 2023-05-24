356becc15ed492c5b06d5ee1649db2b304cc427b
rashadheydarli <rashadahe@code.edu.az>
2023-05-24 16:10:46 +0400

    table task added

---
+++ tableNET/UserController/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace tableNET.UserController
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Rashad";
+            ViewData["Surname"] = "Heydarli";
+            ViewData["Age"] = 27;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = +9941111111;
+            
+            return View();
+        }
+    }
+}
+
